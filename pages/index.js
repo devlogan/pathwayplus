5b1749e5f8c3c8e9f83277c8ce8b03c01b430ee7
Divyanshu Srivastava <dsrivastava65@gmail.com>
2024-02-20 20:09:27 +0530

    next js, react, react router, tailwind initial setup

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-orange-500">
+      <h1 className="text-3xl font-bold ">Hello World!</h1>
+    </div>
+  );
+};
+
+export default Index;
